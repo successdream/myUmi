953898dd9ec3e3e8f0a815256fbd21bbe3fb6e33
喻波 <1142493958@qq.com>
2021-09-01 17:47:00 +0800

    feat: 添加nodeModules到.ignore

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'D:/umi/um3/node_modules/umi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
